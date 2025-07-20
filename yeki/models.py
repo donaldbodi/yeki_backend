b184c1166b740ad36dd7644a20df6e90b941e3b6
bodidonald <mdonabodi99@gmail.com>
2025-07-19 20:52:44 -0700

    v-1.2

--- yeki/models.py
+++ yeki/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('apprenant', 'Apprenant'),
+        ('enseignant', 'Enseignant'),
+        ('enseignant_principal', 'Enseignant Principal'),
+        ('admin', 'Administrateur'),
+    )
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, default='apprenant')
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+
--- yeki_backend/settings.py
+++ yeki_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
     'yeki'
 ]
 
@@ -70,6 +71,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "yeki_backend.wsgi.application"
+
+AUTH_USER_MODEL = 'yeki.CustomUser'
 
 
 # Database
