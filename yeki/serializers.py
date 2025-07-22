6ec734134afef5737e8753579f9194b445b4945a
bodidonald <mdonabodi99@gmail.com>
2025-07-21 18:17:23 -0700

    v-1.4

--- yeki/serializers.py
+++ yeki/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
@@ -16,3 +18,25 @@ class RegisterSerializer(serializers.ModelSerializer):
             role=validated_data.get('role', 'apprenant')
         )
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    identifier = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        identifier = data.get('identifier')
+        password = data.get('password')
+        user = authenticate(username=identifier, password=password)
+
+        if user is None:
+            try:
+                user_obj = User.objects.get(email=identifier)
+                user = authenticate(username=user_obj.username, password=password)
+            except User.DoesNotExist:
+                pass
+
+        if user is None:
+            raise serializers.ValidationError("Identifiants invalides.")
+        data['user'] = user
+        return data
--- yeki/views.py
+++ yeki/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+
+from .serializers import RegisterSerializer, LoginSerializer
 
 class RegisterView(APIView):
     def post(self, request):
@@ -11,5 +16,22 @@ class RegisterView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response({'message': 'Inscription réussie'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'user': {
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email
+                }
+            }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
