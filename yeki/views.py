e99c9f07df4b22cf4aad8b782bb6def2529171b2
bodidonald <mdonabodi99@gmail.com>
2025-07-22 16:17:26 -0700

    v-2.7

--- yeki/views.py
+++ yeki/views.py
@@ -13,13 +13,6 @@ from .serializers import RegisterSerializer, LoginSerializer
 def landing(request):
     return render(request, 'landing-page.html')
 
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'message': 'Inscription réussie'}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class RegisterView(APIView):
     def post(self, request):
