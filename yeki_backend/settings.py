a493721eccdc0ff812517792953aa9cb52f3c4c1
bodidonald <mdonabodi99@gmail.com>
2025-07-25 11:54:39 -0700

    v-5.2

--- yeki_backend/settings.py
+++ yeki_backend/settings.py
@@ -60,10 +60,14 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 
