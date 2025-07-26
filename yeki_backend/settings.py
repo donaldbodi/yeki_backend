6d2b71177b97a237292b20a1d05588a4c6d29771
bodidonald <mdonabodi99@gmail.com>
2025-07-26 09:44:43 -0700

    v-6.6

--- yeki_backend/settings.py
+++ yeki_backend/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['yekiapp.pythonanywhere.com']
 
 CORS_ALLOWED_ORIGINS = [
     "https://yekiapp.pythonanywhere.com",
-    "http://localhost:58304" #lien de production
+    "http://localhost:63680" #lien de production
 ]
 
 
