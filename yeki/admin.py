22b6321bf39a9f577e5f7bad00d4ede205880154
bodidonald <mdonabodi99@gmail.com>
2025-11-07 18:35:26 -0800

    v-1.6.4

--- yeki/admin.py
+++ yeki/admin.py
@@ -4,7 +4,6 @@ from .models import *
 
 admin.site.register(CustomUser)
 admin.site.register(Parcours)
-admin.site.register(AppVersion)
 admin.site.register(Departement)
 admin.site.register(Cours)
 admin.site.register(Lecon)
