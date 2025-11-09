0a50cf2d8858505c99abbcb6bc5fd8dac3d66175
bodidonald <mdonabodi99@gmail.com>
2025-11-09 12:25:11 +0100

    v1.6.8

--- yeki/admin.py
+++ yeki/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from .models import *
 
 admin.site.register(CustomUser)
@@ -8,3 +8,14 @@ admin.site.register(Departement)
 admin.site.register(Cours)
 admin.site.register(Lecon)
 
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_superuser')
+    ordering = ('email',)
+    search_fields = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active', 'groups', 'user_permissions')}),
+        ('Infos personnelles', {'fields': ('first_name', 'last_name')}),
+    )
