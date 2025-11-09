11fdd6e8a5686cb92e3699dac2e3b5d427628499
bodidonald <mdonabodi99@gmail.com>
2025-11-09 17:05:14 +0100

    v-1.6.9

--- yeki_backend/settings.py
+++ yeki_backend/settings.py
@@ -26,10 +26,10 @@ SECRET_KEY = "django-insecure-q^_-_xu=%8i54x&65v*nm6u$@c9y=avyrhyfnu!zb%)l6msm+8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['yekibackend.pythonanywhere.com']
+ALLOWED_HOSTS = ['Yekibackend.pythonanywhere.com']
 
 CORS_ALLOWED_ORIGINS = [
-    "https://yekibackend.pythonanywhere.com",
+    "https://Yekibackend.pythonanywhere.com",
     "http://localhost:61135",
 ]
 
