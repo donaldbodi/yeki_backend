7ab4790eeee78e8f25ab7dabd7703b1b5eeb1817
bodidonald <mdonabodi99@gmail.com>
2025-11-27 16:44:07 -0800

    v-1.7.2

--- yeki/apps.py
+++ yeki/apps.py
@@ -1,11 +1,9 @@
 from django.apps import AppConfig
 
-from yeki_backend import yeki
-
 
 class YekiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "yeki"
 
-def ready(self):
-    return yeki.signals
+    def ready(self):
+        return yeki.signals
--- yeki_backend/settings.py
+++ yeki_backend/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-    'yeki',
+    'yeki.apps.YekiConfig',
 ]
 
 MIDDLEWARE = [
