f309dbfb4a352ca2dade0ba8c31da041c1bea56e
bodidonald <mdonabodi99@gmail.com>
2025-12-17 08:37:08 -0800

    v-1.8.8

--- yeki/serializers.py
+++ yeki/serializers.py
@@ -64,10 +64,10 @@ class RegisterSerializer(serializers.Serializer):
             licence=validated_data.get('licence'),
         )
 
-        if profile.user_type == 'Apprenant':
+        if profile.user_type == 'apprenant':
             profile.is_active = True
             profile.save()
-            
+
         return profile
 
 
--- yeki_backend/settings.py
+++ yeki_backend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['yekiapp.pythonanywhere.com']
 
 CORS_ALLOWED_ORIGINS = [
     "https://yekiapp.pythonanywhere.com",
-    "http://localhost:56331",
+    "http://localhost:65421",
 ]
 
 # Application definition
