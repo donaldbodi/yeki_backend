f3ff104a879e8cfdf16244804fbf41c0c9a01a56
bodidonald <mdonabodi99@gmail.com>
2026-03-02 03:42:49 -0800

    v-2.9.1

--- yeki_backend/settings.py
+++ yeki_backend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['yekiapp.pythonanywhere.com']
 
 CORS_ALLOWED_ORIGINS = [
     "https://yekiapp.pythonanywhere.com",
-    "http://localhost:60617",
+    "http://localhost:55496",
 ]
 
 # Application definition
